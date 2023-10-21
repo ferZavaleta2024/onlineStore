f11bd17f95dd3cee8d7fb9b2fcae55fcef20a610
ferZavaleta2024 <144063182+ferZavaleta2024@users.noreply.github.com>
2023-10-21 06:46:20 -0600

    save code

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ const carrito = new CarritoDeCompra();
 
 botonesAgregarAlCarrito.forEach((boton, index) => {
     boton.addEventListener('click', () => {
-        const producto = new Producto(`Producto ${index + 1}`, `Descripción del Producto ${index + 1}`, 10.0);
+        const producto = new Producto(`Producto ${index + 1}`, `Descripción del Producto ${index + 1}`, 30.0);
         carrito.agregarProducto(producto);
 
         // Actualizar la lista del carrito
@@ -45,3 +45,19 @@ realizarOrdenButton.addEventListener('click', () => {
     const total = carrito.calcularTotal();
     alert(`Orden de compra realizada. Total: $${total}`);
 });
+
+// Esta función añade un producto al carrito de compras
+function addToCart(name, price){}
+    // Crear un elemento li con el nombre y el precio del producto
+    let li = document.createElement("li");
+    li.textContent = name + " - $" + price;
+
+    // Añadir el elemento li al elemento ul con id "items"
+    let items = document.getElementById("items");
+    items.appendChild(li);
+
+    // Actualizar el elemento p con id "total" con el nuevo total
+    let total = document.getElementById("total");
+    let currentTotal = Number(total.textContent.split("$")[1]);
+    let newTotal = currentTotal + price;
+    total.textContent = "Total: $" + newTotal;

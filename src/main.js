beaf82849db161a3ed1363f0dc0c8a24c9eb9b62
ferZavaleta2024 <144063182+ferZavaleta2024@users.noreply.github.com>
2023-10-25 23:58:42 -0600

    Code change $ add idea and code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
- // Clase Producto
- class Producto {
+
+// Clase Producto
+class Producto {
     constructor(nombre, descripcion, precio) {
         this.nombre = nombre;
         this.descripcion = descripcion;
@@ -13,51 +14,83 @@ class CarritoDeCompra {
         this.productos = [];
     }
 
-    agregarProducto(producto) {
-        this.productos.push(producto);
+    agregarProducto(producto, cantidad) {
+        const productoExistente = this.productos.find(p => p.producto.nombre === producto.nombre);
+        if (productoExistente) {
+            productoExistente.cantidad += cantidad;
+        } else {
+            this.productos.push({ producto, cantidad });
+        }
     }
 
     calcularTotal() {
-        return this.productos.reduce((total, producto) => total + producto.precio, 0);
+        return this.productos.reduce((total, item) => total + item.producto.precio * item.cantidad, 0);
     }
 }
 
-// Evento para agregar productos al carrito
+// ...
+
+/// Evento para agregar productos al carrito
 const botonesAgregarAlCarrito = document.querySelectorAll('.bg-blue-500');
 const carrito = new CarritoDeCompra();
 
 botonesAgregarAlCarrito.forEach((boton, index) => {
     boton.addEventListener('click', () => {
-        const producto = new Producto(`Producto ${index + 1}`, `Descripción del Producto ${index + 1}`, 30.0);
-        carrito.agregarProducto(producto);
+        const producto = new Producto(`Producto ${index + 1}`, `Descripción del Producto ${index + 1}`, 50.0);
+        carrito.agregarProducto(producto, 1); // Agrega 1 unidad del producto
 
-        // Actualizar la lista del carrito
+        // Actualizar la lista del carrito y calcular el total
         const listaCarrito = document.getElementById('carrito');
-        const li = document.createElement('li');
-        li.textContent = `${producto.nombre} - $${producto.precio}`;
-        listaCarrito.appendChild(li);
+        listaCarrito.innerHTML = ''; // Limpiar la lista antes de agregar los elementos actualizados
+
+        let total = 0; // Inicializar el total en 0
+
+        carrito.productos.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = `${item.producto.nombre} - Cantidad: ${item.cantidad} - $${item.producto.precio * item.cantidad}`;
+            listaCarrito.appendChild(li);
+
+            // Sumar al total
+            total += item.producto.precio * item.cantidad;
+        });
+
+        // Mostrar el total
+        const totalElement = document.createElement('li');
+        totalElement.textContent = `Total: $${total}`;
+        listaCarrito.appendChild(totalElement);
     });
 });
 
-// Evento para realizar una orden de compra
-const realizarOrdenButton = document.getElementById('realizarOrden');
-realizarOrdenButton.addEventListener('click', () => {
-    const total = carrito.calcularTotal();
-    alert(`Orden de compra realizada. Total: $${total}`);
-});
 
-// Esta función añade un producto al carrito de compras
-function addToCart(name, price){}
-    // Crear un elemento li con el nombre y el precio del producto
-    let li = document.createElement("li");
-    li.textContent = name + " - $" + price;
+// ...
 
-    // Añadir el elemento li al elemento ul con id "items"
-    let items = document.getElementById("items");
-    items.appendChild(li);
 
-    // Actualizar el elemento p con id "total" con el nuevo total
-    let total = document.getElementById("total");
-    let currentTotal = Number(total.textContent.split("$")[1]);
-    let newTotal = currentTotal + price;
-    total.textContent = "Total: $" + newTotal;
+//Trabajar mañana jueves!
+
+    class RopaSection {
+        constructor(id) {
+            this.id = id;
+            this.tabButton = document.getElementById(`${id}-tab`);
+            this.content = document.getElementById(`${id}-content`);
+            this.tabButton.addEventListener("click", () => this.showContent());
+        }
+    
+        showContent() {
+            this.tabButton.classList.add("active");
+            if (this.id === "mujeres") {
+                hombresSection.hideContent();
+            } else if (this.id === "hombres") {
+                mujeresSection.hideContent();
+            }
+            this.content.classList.remove("hidden");
+        }
+    
+        hideContent() {
+            this.tabButton.classList.remove("active");
+            this.content.classList.add("hidden");
+        }
+    }
+    
+    const mujeresSection = new RopaSection("mujeres");
+    const hombresSection = new RopaSection("hombres");
+    
